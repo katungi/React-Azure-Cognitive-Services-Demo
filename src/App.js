9b31a3bd128cf9279a774afc1b5ee94bc59313c6
katungi <dankatdennis@gmail.com>
2020-11-29 22:33:36 +0300

    added language analysis

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import Textfield from "@atlaskit/textfield";
-import Button from "@atlaskit/button";
+
 
 function App() {
   const [text, setText] = useState("");
   const [sentiment, setSentiment] = useState("");
+  const [mode, setMode] = useState("");
+  const [language, setLanguage] = useState("");
 
   const onChangeHandler = (event) => {
     setText(event.target.value);
   };
-   
+  const onOptionSelect = (event)=> {
+    setMode(event.target.value);
+  }
+    const options = [
+      {
+        label: "Languages",
+        value: "languages",
+      },
+      {
+        label: "Sentiment",
+        value: "sentiment",
+      }
+    ];
 
   useEffect(() => {
     axios({
       method: "post",
       url:
-        "https://skeshy.cognitiveservices.azure.com/text/analytics/v3.0/sentiment",
+        `https://skeshy.cognitiveservices.azure.com/text/analytics/v3.0/${mode}`,
       data: {
         documents: [
           {
@@ -30,15 +44,17 @@ function App() {
       headers: {
         "Ocp-Apim-Subscription-Key": "a9ac1ff619c94b028020e87ef413708e",
         "Content-Type": "application/json",
-        Accept: "application/json",
+        "Accept": "application/json",
       },
     })
       .then((res) => {
         console.log(res.data.documents[0].sentiment);
         setSentiment(res.data.documents[0].sentiment);
+        console.log(res.data.documents[0].detectedLanguage.name);
+        setLanguage(res.data.documents[0].detectedLanguage.name);
       })
       .catch((err) => console.log(err));
-  }, [text]);
+  }, [text,mode]);
 
   return (
     <div className="App">
@@ -52,10 +68,15 @@ function App() {
       />
       <br />
       <br />
-      <Button>Submit</Button>
+      <select value={mode} onChange={onOptionSelect}>
+            {options.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
       <br />
       <br />
-      <p>{sentiment !== undefined ? sentiment : "waiting..."}</p>
+      <p>{(sentiment !== undefined) ? sentiment : ""}</p>
+      <p>{(language !== undefined) ? language : ""}</p>
     </div>
   );
 }
