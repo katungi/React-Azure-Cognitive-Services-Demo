6c0642d2fda4b186cb92bd3d506afc447f749827
katungi <dankatdennis@gmail.com>
2020-12-03 13:05:40 +0300

    ruheni crashed the app ✖

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-const dotenv  = require('dotenv');
+import { debounce } from "lodash";
 
 function App() {
   const [text, setText] = useState("");
@@ -9,11 +9,9 @@ function App() {
   const [mode, setMode] = useState("languages");
   const [language, setLanguage] = useState("");
   const [languageConfidence, setConfidence] = useState("");
-  const [negConfidence, setNegative] = useState('');
-  const [posConfidence, setPositive] = useState('');
-  const [neutralConfidence, setNeutral] = useState('');
 
-  const env = dotenv.config();
+  const [document, setDocuments] = useState({});
+
   const onChangeHandler = (event) => {
     setText(event.target.value);
   };
@@ -32,38 +30,39 @@ function App() {
   ];
 
   useEffect(() => {
-    axios({
-      method: "post",
-      url: `https://codeless.cognitiveservices.azure.com/text/analytics/v3.0/${mode}`,
-      data: {
-        documents: [
-          {
-            language: "en",
-            id: "1",
-            text: text,
-          },
-        ],  
-      },
-      headers: {
-        "Ocp-Apim-Subscription-Key": env.SECRET_KEY,
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-    })
-      .then((res) => {
-        console.log(res.data.documents[0].sentiment);
-        setSentiment(res.data.documents[0].sentiment);
-        console.log(res.data.documents[0].detectedLanguage.name);
-        setLanguage(res.data.documents[0].detectedLanguage.name);
-        setConfidence(res.data.documents[0].detectedLanguage.confidenceScore);
-        console.log(res.data.documents[0].confidenceScores);
+    const debouncedSave = debounce(() => makeRequest(), 1000);
+    debouncedSave();
 
-        setNeutral(res.data.documents[0].confidenceScores.neutral);
-        setPositive(res.data.documents[0].confidenceScores.positive);
-        setNegative(res.data.documents[0].confidenceScores.negative);
-        
-      })
-      .catch((err) => console.log(err));
+    const makeRequest = () => {
+      axios({
+        method: "post",
+        url: `https://codeless.cognitiveservices.azure.com/text/analytics/v3.0/${mode}`,
+        data: {
+          documents: [
+            {
+              language: "en",
+              id: "1",
+              text: text,
+            },
+          ],
+        },
+        headers: {
+          "Ocp-Apim-Subscription-Key": `${process.env.REACT_APP_KEY}`,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })  
+        .then(({ data }) => {
+          setDocuments(data.documents[0]);
+          console.log(document.confidenceScores.positive);
+
+          setSentiment(data.documents[0].sentiment);
+          setLanguage(data.documents[0].detectedLanguage.name);
+          setConfidence(data.documents[0].detectedLanguage.confidenceScore);
+        console.log(document);
+        })
+        .catch((err) => console.log(err));
+    };
   }, [text, mode]);
 
   return (
@@ -98,22 +97,32 @@ function App() {
       <h2>Processed Output</h2>
       <p>{sentiment !== undefined ? `Sentiment: ${sentiment}` : ""}</p>
       <p>{language !== undefined || null ? `Language: ${language}` : ""}</p>
-      <br/>
-      <br/>
+      <br />
+      <br />
       <label>Sentiment Confidence Scores</label>
-      <p>{sentiment !== undefined ? `Positive Score: ${neutralConfidence}` : ""}</p>
-      <p>{sentiment !== undefined ? `Neutral Score: ${posConfidence}` : ""}</p>
-      <p>{sentiment !== undefined ? `Negative Score: ${negConfidence}` : ""}</p>
-      
+      <p>
+        {document !== undefined
+          ? `Positive Score: `
+          : ""}
+      </p>
+      <p>
+        {document !== undefined
+          ? `Neutral Score: `
+          : ""}
+      </p>
+      <p>
+        {document !== undefined
+          ? `Negative Score: `
+          : ""}
+      </p>
+
       <label>Language Confidence Scores</label>
-    
+
       <p>
         {language !== undefined
           ? `Confidence Score: ${languageConfidence}`
           : ""}
       </p>
-         
-      
     </div>
   );
 }
